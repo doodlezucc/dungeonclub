0a042cf33b4964e64ea66ffdf38c407bc6c8ce85
doodlezucc <electerminator@gmail.com>
2021-02-10 02:26:39 +0100

    zoomable board

--- web/dart/session/board.dart
+++ web/dart/session/board.dart
@@ -10,19 +10,25 @@ class Board {
   Point<num> get position => _position;
   set position(Point<num> pos) {
     _position = pos;
-    e.style.left = '${pos.x}px';
-    e.style.top = '${pos.y}px';
+    _transform();
   }
 
-  double _zoom;
+  double _zoom = 0;
+  double _scaledZoom = 1;
   double get zoom => _zoom;
-  set zoom(double z) {
-    _zoom = z;
+  set zoom(double zoom) {
+    _zoom = zoom;
+    _scaledZoom = exp(zoom);
+    _transform();
+  }
+
+  void _transform() {
+    e.style.transform =
+        'scale($_scaledZoom) translate(${position.x}px, ${position.y}px)';
   }
 
   Board() {
     position = Point(0, 0);
-    zoom = 1;
 
     var drag = false;
     container.onMouseDown.listen((event) async {
@@ -32,8 +38,12 @@ class Board {
     });
     window.onMouseMove.listen((event) {
       if (drag) {
-        position += event.movement;
+        position += event.movement * (1 / _scaledZoom);
       }
+    });
+
+    container.onMouseWheel.listen((event) {
+      zoom -= event.deltaY / 300;
     });
   }
 }
