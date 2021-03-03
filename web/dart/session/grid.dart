80abad6cc6c255cc3df9025c5ecf5cb814544d0c
doodlezucc <electerminator@gmail.com>
2021-03-03 02:43:54 +0100

    sharpened board grid

--- web/dart/session/grid.dart
+++ web/dart/session/grid.dart
@@ -35,11 +35,11 @@ class Grid {
     var ctx = _canvas.context2D;
     ctx.clearRect(0, 0, _canvas.width, _canvas.height);
     ctx.setStrokeColorRgb(0, 0, 0);
-    for (var x = 0.0; x <= _canvas.width; x += _cellSize) {
+    for (var x = 0.5; x <= _canvas.width; x += _cellSize) {
       ctx.moveTo(x, 0);
       ctx.lineTo(x, _canvas.height);
     }
-    for (var y = 0.0; y <= _canvas.height; y += _cellSize) {
+    for (var y = 0.5; y <= _canvas.height; y += _cellSize) {
       ctx.moveTo(0, y);
       ctx.lineTo(_canvas.width, y);
     }
