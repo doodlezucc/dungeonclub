230a0e313c665ec6ca979f5ca1d293a5a8214193
Nuhamaru <lukas.christopher.haefele@gmail.com>
2021-07-28 01:26:31 +0200

    Initiative is forced to roll at button click. Result is given to only the roller in Chatbox

--- web/dart/session/initiative_tracker.dart
+++ web/dart/session/initiative_tracker.dart
@@ -14,50 +14,45 @@ void initInitiativeTracker() {
   querySelector('#initiativeTracker')?.onClick?.listen((event) {
     rollForInitiative();
   });
+  //print('Roll for Initiative');
+  querySelector('.initiativeRollButton').onClick.listen((event) {
+    ranResInChat();
+  });
 }
 
 void rollForInitiative() {
-  for (int i = 0; i < user.session.board.movables.length; i++) {
+  /*for (int i = 0; i < user.session.board.movables.length; i++) {
     Prefab prefab = user.session.board.movables[i].prefab;
     if (prefab is CharacterPrefab) {
       Character character = prefab.character;
       socket.sendAction(GAME_ROLL_INITIATIVE);
     }
-  }
+  }*/
+  socket.sendAction(GAME_ROLL_INITIATIVE);
 }
 
 void ranResInChat() {
-  int mod = querySelector('#initiativeMod').nodeValue as int;
+  if ((querySelector('#initiativeMod') as InputElement).value == '') return;
+  int mod =
+      (querySelector('#initiativeMod') as InputElement).valueAsNumber.round();
   /*var results ={
     'sides': 20,
     'id': user.session.charId,
   };*/
   int initiator = user.session.charId;
-  var name = user.session.characters[initiator].name;
   var mine = initiator == null || initiator == user.session.charId;
   int r = Random().nextInt(19) + 1;
   int total = r + mod;
   gameLog('''
-    $name rolled $r with an initiative modifier of $mod for a total of $total for initiative''',
+    You rolled $r with an initiative modifier of $mod for a total of $total for initiative''',
       mine: mine);
-}
-
-void randomStrudel() {
-  while (true) {
-    int r = Random().nextInt(19) + 1;
-    querySelector('.initiativeRollButton').text = r as String;
-  }
+  querySelector('#initiativeRoll').classes.remove('show');
+  overlayVisible = false;
 }
 
 void showRollerPanel() {
-  //querySelector('#initiativeRoll').classes.add('show');
-  //overlayVisible = true;
-  querySelector('.initiativeRollButton')
-    ..onMouseOver.listen((event) {
-      randomStrudel();
-    })
-    ..onClick.listen((event) {
-      ranResInChat();
-    });
+  querySelector('#initiativeRoll').classes.add('show');
+  (querySelector('#intitativeMod') as InputElement).value = '0';
+  overlayVisible = true;
   print('Show Roller Panel');
 }
