9ecf3dc8ae1dd55fa03709d7db4812d6ebf5292a
doodlezucc <electerminator@gmail.com>
2022-01-31 23:01:12 +0100

    increased limit on tokens per scene

--- lib/limits.dart
+++ lib/limits.dart
@@ -2,5 +2,5 @@ const campaignsPerAccount = 10;
 const scenesPerCampaign = 10;
 const prefabsPerCampaign = 20;
 const mapsPerCampaign = 10;
-const movablesPerScene = 50;
+const movablesPerScene = 500;
 const playersPerCampaign = 20;
