8c34a0de6495a0e039ef971888714a0d345b5f5c
doodlezucc <electerminator@gmail.com>
2023-07-30 16:48:55 +0200

    start webdev server in development launch script

--- bin/dev.dart
+++ bin/dev.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:sass/sass.dart' as sass;
@@ -5,12 +7,63 @@ import './server.dart' as server;
 
 void main(List<String> args) async {
   await startScssWatchCycle();
+  await startWebdevServer();
+
+  print(''); // Empty line
 
   server.main(args);
 }
 
-void _devPrint(dynamic message) {
-  print('[Dev] $message');
+void _devPrint(dynamic message, String process) {
+  final prefix = '[Dev: $process]';
+  final prefixedMessage =
+      '$message'.split('\n').map((line) => '$prefix $line').join('\n');
+
+  print(prefixedMessage);
+}
+
+Future<void> startWebdevServer() async {
+  final process = await Process.start('dart', [
+    'pub',
+    'global',
+    'run',
+    'webdev',
+    'serve',
+  ]);
+
+  final whenReady = Completer<bool>();
+
+  // Fail if the program exits before the first compile success
+  process.exitCode.then((_) {
+    if (!whenReady.isCompleted) {
+      whenReady.complete(false);
+    }
+  });
+
+  process.stdout.listen((data) {
+    if (!whenReady.isCompleted) {
+      stdout.add(data);
+
+      final message = utf8.decode(data);
+      if (message.contains('Succeeded after')) {
+        whenReady.complete(true);
+      }
+    }
+  });
+
+  process.stderr.listen((data) {
+    final message = utf8.decode(data);
+    _devPrint(message, 'Web');
+  });
+
+  _devPrint('Starting webdev server...\n', 'Web');
+  final isReady = await whenReady.future;
+
+  if (isReady) {
+    _devPrint('\nWebsite serving at http://localhost:8080', 'Web');
+  } else {
+    throw 'Unable to start webdev server';
+  }
 }
 
 Future<void> startScssWatchCycle() async {
@@ -25,14 +78,15 @@ Future<void> startScssWatchCycle() async {
   var isCompiling = false;
 
   sassDirectory.watch(recursive: true).forEach((fse) async {
-    if (isCompiling) return;
+    if (!isCompiling) {
+      isCompiling = true;
 
-    isCompiling = true;
-    await _compileScssToCss(src, dstFile);
-    isCompiling = false;
+      await _compileScssToCss(src, dstFile);
+      isCompiling = false;
+    }
   });
 
-  _devPrint('Watching for stylesheet changes');
+  _devPrint('Watching for stylesheet changes', 'CSS');
 }
 
 Future<void> _compileScssToCss(String src, File dstFile) async {
