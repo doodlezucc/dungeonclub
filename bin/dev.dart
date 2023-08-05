623db5e004b8c189c5ca1d221e46226790b4f2fa
doodlezucc <electerminator@gmail.com>
2023-08-05 15:14:39 +0200

    move dev script processes into separate classes

--- bin/dev.dart
+++ bin/dev.dart
@@ -6,155 +6,193 @@ import 'dart:isolate';
 import 'package:sass/sass.dart' as sass;
 
 void main(List<String> args) async {
-  // await startScssWatchCycle();
-  // await startWebdevServer();
+  final stylesheetProcess = StylesheetProcess();
+  final webdevProcess = WebdevProcess();
+  final backendProcess = BackendProcess(args);
+
+  await stylesheetProcess.startCycle();
+  // await webdevProcess.startCycle();
 
   print(''); // Empty line
 
-  await startBackendServerCycle(args);
+  // await backendProcess.startCycle();
 }
 
-void _devPrint(dynamic message, String process) {
-  final prefix = '[Dev: $process]';
-  final prefixedMessage =
-      '$message'.split('\n').map((line) => '$prefix $line').join('\n');
+abstract class DevProcess {
+  final String name;
 
-  print(prefixedMessage);
-}
+  DevProcess(this.name);
 
-/// Starts the backend server (at "bin/server.dart") in a new isolate process.
-/// Returns the isolate after the server is fully started.
-Future<Isolate> _startNewBackendIsolate(List<String> args) async {
-  final completer = Completer();
+  void devPrint(dynamic message) {
+    final prefix = '[Dev: $name]';
+    final prefixedMessage =
+        '$message'.split('\n').map((line) => '$prefix $line').join('\n');
 
-  // Wait for a message to be sent to the receiver port.
-  final receiver = ReceivePort();
-  receiver.first.then(completer.complete);
-
-  final isolate = await Isolate.spawnUri(
-    Uri.file('./server.dart'),
-    args,
-    receiver.sendPort,
-  );
-
-  await completer.future;
-
-  return isolate;
-}
-
-/// Starts the backend server and listens to stdin for control keypresses.
-Future<void> startBackendServerCycle(List<String> args) async {
-  final debugName = 'Backend';
-  var isRestarting = false;
-
-  _devPrint('Spawning backend server isolate...', debugName);
-  var serverIsolate = await _startNewBackendIsolate(args);
-
-  void restart() async {
-    if (isRestarting) return;
-
-    _devPrint('Restarting...', debugName);
-    isRestarting = true;
-    serverIsolate.kill();
-
-    serverIsolate = await _startNewBackendIsolate(args);
-
-    isRestarting = false;
+    print(prefixedMessage);
   }
 
-  stdin.echoMode = false;
-  stdin.lineMode = false;
-
-  stdin.listen((data) async {
-    final char = String.fromCharCodes(data)[0];
-
-    switch (char.toLowerCase()) {
-      case 'r':
-        restart();
-        break;
-    }
-  });
-
-  _devPrint('Press [R] to restart the backend server', debugName);
+  Future<void> startCycle();
 }
 
-Future<void> startWebdevServer() async {
-  final debugName = 'Web';
-  final process = await Process.start('dart', [
-    'pub',
-    'global',
-    'run',
-    'webdev',
-    'serve',
-  ]);
+class BackendProcess extends DevProcess {
+  final List<String> _args;
+  Isolate? _serverIsolate;
 
-  final whenReady = Completer<bool>();
+  bool get isStarting => _serverIsolate != null;
 
-  // Fail if the program exits before the first compile success
-  process.exitCode.then((_) {
-    if (!whenReady.isCompleted) {
-      whenReady.complete(false);
-    }
-  });
+  BackendProcess(List<String> args)
+      : _args = args,
+        super('Backend');
 
-  process.stdout.listen((data) {
-    if (!whenReady.isCompleted) {
-      stdout.add(data);
+  /// Starts the backend server (at "bin/server.dart") in a new isolate process.
+  /// Returns the isolate after the server is fully started.
+  Future<Isolate> _startNewBackendIsolate() async {
+    devPrint('Spawning backend server isolate...');
+    final completer = Completer();
 
-      final message = utf8.decode(data);
-      if (message.contains('Succeeded after')) {
-        whenReady.complete(true);
-      }
-    }
-  });
+    // Wait for a message to be sent to the receiver port.
+    final receiver = ReceivePort();
+    receiver.first.then(completer.complete);
 
-  process.stderr.listen((data) {
-    final message = utf8.decode(data);
-    _devPrint(message, debugName);
-  });
-
-  _devPrint('Starting webdev server...\n', debugName);
-  final isReady = await whenReady.future;
-
-  if (isReady) {
-    _devPrint('\nWebsite serving at http://localhost:8080', debugName);
-  } else {
-    throw 'Unable to start webdev server';
-  }
-}
-
-Future<void> startScssWatchCycle() async {
-  final sassDirectory = Directory('web/sass');
-  final src = '${sassDirectory.path}/style.scss';
-
-  final dst = 'web/style/style.css';
-  final dstFile = File(dst);
-
-  await _compileScssToCss(src, dstFile);
-
-  var isCompiling = false;
-
-  sassDirectory.watch(recursive: true).forEach((fse) async {
-    if (!isCompiling) {
-      isCompiling = true;
-
-      await _compileScssToCss(src, dstFile);
-      isCompiling = false;
-    }
-  });
-
-  _devPrint('Watching for stylesheet changes', 'CSS');
-}
-
-Future<void> _compileScssToCss(String src, File dstFile) async {
-  try {
-    final compileResult = sass.compileToResult(
-      src,
-      style: sass.OutputStyle.compressed,
+    final isolate = await Isolate.spawnUri(
+      Uri.file('./server.dart'),
+      _args,
+      receiver.sendPort,
     );
 
-    await dstFile.writeAsString(compileResult.css);
-  } on sass.SassException catch (error) {
-    print('\n$error\n');
+    await completer.future;
+
+    return isolate;
+  }
+
+  /// Kills the active server isolate process.
+  void killIsolate() {
+    if (_serverIsolate != null) {
+      _serverIsolate!.kill();
+      _serverIsolate = null;
+
+      devPrint('Exited server');
+    }
+  }
+
+  /// Kills the active isolate and waits for a new one to start.
+  Future<void> restart() async {
+    if (isStarting) return;
+
+    killIsolate();
+    _serverIsolate = await _startNewBackendIsolate();
+  }
+
+  /// Starts the backend server and listens to stdin for control keypresses.
+  Future<void> startCycle() async {
+    await _startNewBackendIsolate();
+
+    stdin.echoMode = false;
+    stdin.lineMode = false;
+
+    stdin.listen((data) async {
+      final char = String.fromCharCodes(data)[0];
+
+      switch (char.toLowerCase()) {
+        case 'r':
+          restart();
+          break;
+      }
+    });
+
+    devPrint('Press [R] to restart the backend server');
+  }
+}
+
+class WebdevProcess extends DevProcess {
+  WebdevProcess() : super('Web');
+
+  @override
+  Future<void> startCycle() async {
+    final process = await Process.start('dart', [
+      'pub',
+      'global',
+      'run',
+      'webdev',
+      'serve',
+    ]);
+
+    final whenReady = Completer<bool>();
+
+    // Fail if the program exits before the first compile success
+    process.exitCode.then((_) {
+      if (!whenReady.isCompleted) {
+        whenReady.complete(false);
+      }
+    });
+
+    // Pipe webdev's stdout until the first compilation is complete
+    process.stdout.listen((data) {
+      if (!whenReady.isCompleted) {
+        stdout.add(data);
+
+        final message = utf8.decode(data);
+        if (message.contains('Succeeded after')) {
+          whenReady.complete(true);
+        }
+      }
+    });
+
+    // Pipe webdev's stderr
+    process.stderr.listen((data) {
+      final message = utf8.decode(data);
+      devPrint(message);
+    });
+
+    devPrint('Starting webdev server...\n');
+    final isReady = await whenReady.future;
+
+    if (isReady) {
+      devPrint('\nWebsite serving at http://localhost:8080');
+    } else {
+      throw 'Unable to start webdev server';
+    }
+  }
+}
+
+class StylesheetProcess extends DevProcess {
+  bool _isCompiling = false;
+
+  StylesheetProcess() : super('CSS');
+
+  @override
+  Future<void> startCycle() async {
+    final sassDirectory = Directory('web/sass');
+    final src = '${sassDirectory.path}/style.scss';
+
+    final dst = 'web/style/style.css';
+    final dstFile = File(dst);
+
+    await _compileScssToCss(src, dstFile);
+
+    sassDirectory.watch(recursive: true).forEach((fse) async {
+      if (!_isCompiling) {
+        await _compileScssToCss(src, dstFile);
+      }
+    });
+
+    devPrint('Watching for stylesheet changes');
+  }
+
+  Future<void> _compileScssToCss(String src, File dstFile) async {
+    _isCompiling = true;
+
+    try {
+      final compileResult = sass.compileToResult(
+        src,
+        style: sass.OutputStyle.compressed,
+      );
+
+      await dstFile.writeAsString(compileResult.css);
+    } on sass.SassException catch (error) {
+      devPrint('\n$error\n');
+    } finally {
+      _isCompiling = false;
+    }
   }
 }
