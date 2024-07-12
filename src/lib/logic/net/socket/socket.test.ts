200d3a270e201bdf881df00702e6ada09a7e880e
doodlezucc <electerminator@gmail.com>
2024-07-13 00:01:21 +0200

    use correct relationship in prisma schema

--- src/lib/logic/net/socket/socket.test.ts
+++ src/lib/logic/net/socket/socket.test.ts
@@ -14,6 +14,7 @@ import { MessageSocket } from './socket';
 
 it('sends and receives', async () => {
 	const response = await testClient.request('tokenCreate', {
+		boardId: 'boardid',
 		position: {
 			x: 0.5,
 			y: 2.5
