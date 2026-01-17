73853cccce33514010385df2734d9bf283991e48
doodlezucc <electerminator@gmail.com>
2026-01-17 21:13:17 +0100

    Prefer final variables in DungeonClubConfig

--- bin/limits.dart
+++ bin/limits.dart
@@ -7,4 +7,5 @@ final mapsPerCampaign = DungeonClubConfig.mapsPerCampaign;
 const tokensPerScene = 500;
 const playersPerCampaign = 20;
 
-final mediaBytesPerCampaign = DungeonClubConfig.storageMegabytesPerCampaign * 1000 * 1000;
+final mediaBytesPerCampaign =
+    DungeonClubConfig.storageMegabytesPerCampaign * 1000 * 1000;
