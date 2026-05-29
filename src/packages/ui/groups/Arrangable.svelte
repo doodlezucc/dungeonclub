afd187620cd161d0d9f930f7070e8a5484ed19eb
doodlezucc <electerminator@gmail.com>
2026-05-29 15:18:58 +0200

    Revert store/state change in Arrangable.svelte

--- src/packages/ui/groups/Arrangable.svelte
+++ src/packages/ui/groups/Arrangable.svelte
@@ -32,7 +32,7 @@
 	let container = $state<HTMLElement>();
 
 	$effect(() => {
-		if (!isDragging) {
+		if (!$isDragging) {
 			mouseOffset = undefined;
 			draggedCenter = center;
 		}
@@ -44,14 +44,14 @@
 			x: rect.left + rect.width / 2,
 			y: rect.top + rect.height / 2
 		};
-		if (!isDragging) {
+		if (!$isDragging) {
 			visualCenter.set(center);
 		}
 		dragState.setItemCenter(center);
 	}
 
 	$effect(() => {
-		if (container && isAnyDragging && index !== undefined) {
+		if (container && $isAnyDragging && index !== undefined) {
 			untrack(() => findCenter());
 		}
 	});
@@ -93,11 +93,11 @@
 	}
 </script>
 
-<svelte:window onpointermove={isDragging ? handleMouseMove : undefined} />
+<svelte:window onpointermove={$isDragging ? handleMouseMove : undefined} />
 
 <div
 	class="arrangable"
-	class:dragging={isDragging}
+	class:dragging={$isDragging}
 	role="listitem"
 	draggable="true"
 	ondragstart={onDragContainer}
